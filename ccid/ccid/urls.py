c6b8a871b43c2ccf71ff4ba28b4775c50cfeb9fc
Doggra <doggra@protonmail.com>
2017-05-23 18:36:05 +0200

    initial homepage

--- ccid/ccid/urls.py
+++ ccid/ccid/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ccid.views import Home
 
 urlpatterns = [
+	url(r'^$', Home.as_view(), name='home'),
+
     url(r'^admin/', admin.site.urls),
 ]
