0ff25f954c72841464e4573a6981eb8a6a682385
Doggra <doggra@protonmail.com>
2017-05-30 16:34:01 +0200

    specify min and max values to form fields

--- ccid/ccid/forms.py
+++ ccid/ccid/forms.py
@@ -6,14 +6,14 @@ from crawler.models import Quarter, Meridian, Crop, Deductible
 
 class LandLocationForm(forms.Form):
     quarter = forms.ModelChoiceField(queryset=Quarter.objects.all())
-    section = forms.CharField()
-    township = forms.CharField()
-    _range = forms.IntegerField(label='Range')
+    section = forms.CharField(max_length=2)
+    township = forms.CharField(max_length=2)
+    _range = forms.IntegerField(max_value=20, label='Range')
     meridian = forms.ModelChoiceField(queryset=Meridian.objects.all())
     deductible = forms.ModelChoiceField(queryset=Deductible.objects.all())
 
 
 class CropInfoForm(forms.Form):
     crop = forms.ModelChoiceField(label='Crop type', queryset=Crop.objects.all())
-    acres = forms.IntegerField()
+    acres = forms.IntegerField(max_value=999, min_value=1)
     coverage = forms.DecimalField(label='Coverage ( $ / Acre )')
