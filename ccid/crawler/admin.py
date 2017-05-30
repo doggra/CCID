6f2c3fe46d8da1617755c0b221d2e9ac034329e1
Doggra <doggra@protonmail.com>
2017-05-30 16:35:32 +0200

    minor syntax changes, add style to about page

--- ccid/crawler/admin.py
+++ ccid/crawler/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Crop, Quarter, Meridian, Deductible, CrawlerRequest
+from .models import Crop, Quarter, Meridian, Deductible, CrawlerRequest, CrawlerResult
 
 
 admin.site.register(Crop)
@@ -10,3 +10,4 @@ admin.site.register(Quarter)
 admin.site.register(Meridian)
 admin.site.register(Deductible)
 admin.site.register(CrawlerRequest)
+admin.site.register(CrawlerResult)
